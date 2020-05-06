626fd54569e418539b460db19c6267337da00750
jamesyjh <yjh0205@gmail.com>
2020-05-06 17:05:37 -0400

    clear repo

--- Models/Comment.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-var CommentSchema = new mongoose.Schema({
-  profile: { type: String, default: "" },
-  text: { type: String, default: "" },
-  timestamp: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("CommentSchema", CommentSchema);
--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-var ProfileController = require("./ProfileController");
-var CommentController = require("./CommentController");
-
-module.exports = {
-  profile: ProfileController,
-  comment: CommentController
-};
